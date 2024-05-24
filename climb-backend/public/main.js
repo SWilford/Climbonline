098d3f46bd5a242f460ae019a3c601965c85189b
Spencer Wilford <113046534+SWilford@users.noreply.github.com>
2024-05-24 17:20:11 +0000

    yuh

--- climb-backend/public/main.js
+++
@@ -1,8 +0,0 @@
-function showLogin() {
-    var loginForm = document.getElementById('loginForm');
-    if (loginForm.style.display === 'none') {
-      loginForm.style.display = 'block';
-    } else {
-      loginForm.style.display = 'none';
-    }
-  }
