e4043a1542f99a12a805b55fa4801b6001e05f03
Spencer Wilford <113046534+SWilford@users.noreply.github.com>
2024-05-24 15:45:53 +0000

    quick

--- climb-backend/index.js
+++ climb-backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000; // Set the port number
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define routes
 app.get('/', (req, res) => {
-  res.send('Welcome to Climb backend!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 // Start the server
 app.listen(PORT, () => {
